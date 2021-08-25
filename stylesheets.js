0e0b488c45b4fa9df5e2b650d1e596e3f9fdf9a1
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-08-25 15:40:30 +0530

    Create stylesheets.js

---
+++ stylesheets.js
@@ -0,0 +1,29 @@
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 20
+    },
+    searchBar:{
+      flexDirection:'row',
+      height:40,
+      width:'auto',
+      borderWidth:0.5,
+      alignItems:'center',
+      backgroundColor:'grey',
+  
+    },
+    bar:{
+      borderWidth:2,
+      height:30,
+      width:300,
+      paddingLeft:10,
+    },
+    searchButton:{
+      borderWidth:1,
+      height:30,
+      width:50,
+      alignItems:'center',
+      justifyContent:'center',
+      backgroundColor:'green'
+    }
+  })
